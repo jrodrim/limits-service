2bdf8744894fa9dc322332b01b914ccf4996404d
jrodrim <jorgecarl.6@gmail.com>
2020-03-07 14:51:17 +0100

    primo commit

---
+++ src/main/java/com/jorge/microservices/limitsservice/Configuration.java
@@ -0,0 +1,33 @@
+package com.jorge.microservices.limitsservice;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration
+{
+	private int	minimun;
+	private int	maximun;
+
+	public int getMinimun()
+	{
+		return minimun;
+	}
+
+	public void setMinimun(int minimun)
+	{
+		this.minimun = minimun;
+	}
+
+	public int getMaximun()
+	{
+		return maximun;
+	}
+
+	public void setMaximun(int maximun)
+	{
+		this.maximun = maximun;
+	}
+
+}
---
+++ src/main/java/com/jorge/microservices/limitsservice/LimitsServiceApplication.java
@@ -0,0 +1,15 @@
+package com.jorge.microservices.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServiceApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+
+}
